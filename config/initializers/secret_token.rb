a272e5a710bf0a2c9b1fcd1a9a308d9cbff72373
Ravi Sathyam <ravi@mint>
2012-02-25 17:31:27 -0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rnd::Application.config.secret_token = '5ed405a13a6ff258ca846be893c83d2ea783bb4bbb84425eed5c30222148df4acbf3d08a18ae31d3383836bbcdf0b221ba9e14ce1e421c7f07f05ba29b9bfde2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Rnd::Application.config.session_store :cookie_store, key: '_rnd_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Rnd::Application.config.session_store :active_record_store
