778bf3a543df3492639ea0ae9275d8e282962231
David R. Albrecht <albrecht.dr@gmail.com>
2012-04-15 12:48:24 -0700

    Added leader image to features...starting to look like an actual site now

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20120318033720) do
+
+  create_table "features", :force => true do |t|
+    t.string   "title"
+    t.datetime "content_date"
+    t.text     "overview"
+    t.text     "body"
+    t.string   "type"
+    t.datetime "created_at",   :null => false
+    t.datetime "updated_at",   :null => false
+  end
 
 end
